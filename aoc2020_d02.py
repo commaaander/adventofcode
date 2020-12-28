bc43a5090a632a439562771d546c50c80a3186ac
Robert Frießleben <robert.friessleben@shd-online.de>
2020-12-28 12:16:47 +0100

    day 2

--- aoc2020_d02.py
+++ aoc2020_d02.py
@@ -9,6 +9,9 @@ def main():
     print('Anzahl valider Passwörter: {0}'.format(
         sum(map(checkPasswordValidity1, inputLines)))
     )
+    print('Anzahl valider Passwörter: {0}'.format(
+        sum(map(checkPasswordValidity2, inputLines)))
+    )
 
 
 def checkPasswordValidity1(line):
@@ -25,6 +28,20 @@ def checkPasswordValidity1(line):
         passwordIsValid = 0
 
     return passwordIsValid
+
+
+def checkPasswordValidity2(line):
+    match = re.search('^([0-9]+)-([0-9]+) ([a-z]): ([a-z]*)$', line)
+
+    firstIndex = int(match.group(1))
+    secondIndex = int(match.group(2))
+    letter = match.group(3)
+    password = match.group(4)
+
+    if len(list(filter(lambda x: x == letter, [password[firstIndex-1], password[secondIndex-1]]))) == 1:
+        return 1
+    else:
+        return 0
 
 
 if __name__ == "__main__":
