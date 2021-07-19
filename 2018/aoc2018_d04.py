67bc3724e81d3353c4e62ef81d756b802005e742
Robert Frießleben <robert.friessleben@shd-online.de>
2021-07-19 16:52:27 +0200

    work in progress solution part 1

--- 2018/aoc2018_d04.py
+++ 2018/aoc2018_d04.py
@@ -3,20 +3,70 @@ import pathlib
 
 
 def main():
-    data = load_data("d04.data")
+    data = load_data("d04_sorted.data")
     print(f"number of data sets: {len(data)}")
-    print(data)
+    # print(data)
 
     print(f"solution part one: {solution1(data)}")
     print(f"solution part two: {solution2(data)}")
 
 
 def solution1(data):
+
+    guard_id = None
+    falls_asleep = None
+    wakes_up = None
+
+    guard_sleep_times = {}
+
+    for event in data:
+
+        match = re.search(
+            "^\[[0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}\] Guard #([0-9]+) begins shift$",
+            event,
+        )
+        try:
+            guard_id = int(match.group(1))
+            falls_asleep = None
+        except AttributeError:
+            pass
+
+        match = re.search(
+            "^\[[0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:([0-9]{2})\] falls asleep$", event
+        )
+        try:
+            falls_asleep = int(match.group(1).lstrip("0"))
+            wakes_up = None
+        except ValueError:
+            falls_asleep = 0
+            wakes_up = None
+        except AttributeError:
+            pass
+
+        match = re.search(
+            "^\[[0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:([0-9]{2})\] wakes up$", event
+        )
+        try:
+            wakes_up = int(match.group(1).lstrip("0"))
+        except ValueError:
+            wakes_up = 0
+        except AttributeError:
+            pass
+
+        if falls_asleep is not None and wakes_up is not None:
+            for minute in range(falls_asleep, wakes_up):
+                try:
+                    guard_sleep_times[f"{guard_id:05d}-{minute}"] += 1
+                except KeyError:
+                    guard_sleep_times[f"{guard_id:05d}-{minute}"] = 1
+
+    print(max(guard_sleep_times, key=guard_sleep_times.get))
     return 0
 
 
 def solution2(data):
     return 0
+
 
 def load_data(filename):
 
