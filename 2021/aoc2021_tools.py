335272fa86f17d23f3ea45d8657508a7628d7619
Robert Frießleben <robert.friessleben@gmail.com>
2021-12-03 17:49:47 +0100

    no need for a contex, better catch missing file error

--- 2021/aoc2021_tools.py
+++ 2021/aoc2021_tools.py
@@ -1,17 +1,20 @@
 import pathlib
+import sys
 
 
 def load_data(file_name: str, split_sep: str = None):
 
-    ret_val = ''
+    ret_val = ""
 
-    with open(f"{pathlib.Path(__file__).parent.absolute()}/data/{file_name}"
-              ) as input_file:
+    try:
+        input_file_name = f"{pathlib.Path(__file__).parent.absolute()}/data/{file_name}"
+        input_file = open(input_file_name)
 
         ret_val = input_file.read()
         if split_sep is not None:
             ret_val = ret_val.split(split_sep)
-
+    except FileNotFoundError as e:
+        sys.exit(e)
     return ret_val
 
 
