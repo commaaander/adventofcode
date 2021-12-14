65e953282df1d14bbc4830eb4fd25c77915be6b1
Robert Frießleben <robert.friessleben@gmail.com>
2021-12-14 11:19:26 +0100

    Daily Template

---
+++ aoc_2021/day_XX.py
@@ -0,0 +1,20 @@
+""" https://adventofcode.com/2021/dayXX """
+
+from adventofcode import LOG
+
+questions = [
+    "",  # noqa: E501
+    "",  # noqa: E501
+]
+
+
+def part_one(**kwargs):
+    LOG.debug(f"part_one({kwargs=})")
+    answer = 0
+    return questions[0], answer
+
+
+def part_two(**kwargs):
+    LOG.debug(f"part_two({kwargs=})")
+    answer = 0
+    return questions[1], answer
