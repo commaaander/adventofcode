02542f9ff28d250f09aa1d038b4aa59b40baf887
Robert Frießleben <robert.friessleben@gmail.com>
2022-12-01 09:28:00 +0100

    fix linting error E741 and use 129er line length

--- 2021/aoc2021_d07.py
+++ 2021/aoc2021_d07.py
@@ -40,7 +40,7 @@ def solution1(**kwargs) -> int:
         max(i for i in kwargs["raw_data"]) + 1,
     ):
         levels.append((level, sum(abs(i - level) for i in kwargs["raw_data"])))
-    return min(levels, key=lambda l: l[1])[1]
+    return min(levels, key=lambda k: k[1])[1]
 
 
 def solution2(**kwargs) -> int:
@@ -52,12 +52,7 @@ def solution2(**kwargs) -> int:
         levels.append(
             (
                 level,
-                int(
-                    sum(
-                        (abs(i - level) * (abs(i - level) + 1)) / 2
-                        for i in kwargs["raw_data"]
-                    )
-                ),
+                int(sum((abs(i - level) * (abs(i - level) + 1)) / 2 for i in kwargs["raw_data"])),
             )
         )
     return min(levels, key=lambda l: l[1])[1]
@@ -65,10 +60,7 @@ def solution2(**kwargs) -> int:
 
 def load_data(debug: bool) -> TextIOWrapper:
     day = re.search(r"aoc.*_(?P<day>d[0-9]{2})", basename(__file__)).group("day")
-    input_file_name = (
-        f"{pathlib.Path(__file__).parent.absolute()}"
-        f"/data/{day}{'_test' if debug else ''}.data"
-    )
+    input_file_name = f"{pathlib.Path(__file__).parent.absolute()}" f"/data/{day}{'_test' if debug else ''}.data"
 
     try:
         input_file = open(input_file_name)
