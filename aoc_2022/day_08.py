701c399c0c05c86dcfef6ae64f8ac0c6d4501998
Robert Frießleben <robert.friessleben@gmail.com>
2023-12-09 22:10:55 +0100

    add 2020 day 8

---
+++ aoc_2022/day_08.py
@@ -0,0 +1,21 @@
+""" https://adventofcode.com/2022/day/8 """
+
+from adventofcode import LOG
+
+questions = [
+    "Consider your map; how many trees are visible from outside the grid?",  # noqa: E501
+    "",  # noqa: E501
+]
+
+
+def part_one(**kwargs):
+    LOG.debug(f"part_one({kwargs=})")
+    answer = 0
+
+    return questions[0], answer
+
+
+def part_two(**kwargs):
+    LOG.debug(f"part_two({kwargs=})")
+    answer = 0
+    return questions[1], answer
