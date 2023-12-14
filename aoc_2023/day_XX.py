a170a87516d46370fb9f185a857750ef2da07423
Robert Frießleben <robert.friessleben@gmail.com>
2023-12-14 14:03:24 +0100

    Update Advent of Code 2023 template

--- aoc_2023/day_XX.py
+++ aoc_2023/day_XX.py
@@ -1,10 +1,14 @@
-""" https://adventofcode.com/2022/day/X """
+"""
+https://adventofcode.com/2023/day/X
 
-from typing import Any, Dict, Tuple
+
+"""
+
+from typing import Any, Dict, List, Tuple
 
 from adventofcode import LOG
 
-questions = [
+questions: List[str] = [
     "",  # noqa: E501
     "",  # noqa: E501
 ]
@@ -12,11 +16,11 @@ questions = [
 
 def part_one(**kwargs: Dict[str, Any]) -> Tuple[str, str]:
     LOG.debug(f"part_one({kwargs=})")
-    answer = 0
+    answer: int = 0
     return questions[0], answer
 
 
 def part_two(**kwargs: Dict[str, Any]) -> Tuple[str, str]:
     LOG.debug(f"part_two({kwargs=})")
-    answer = 0
+    answer: int = 0
     return questions[1], answer
