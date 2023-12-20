47203c91f3b7452d4199ab8aa0a61ab58b679ceb
Robert Frießleben <robert.friessleben@gmail.com>
2023-12-20 08:55:01 +0100

    Move progress tracking to docs

---
+++ aoc_2023/day_19.py
@@ -0,0 +1,22 @@
+""" https://adventofcode.com/2023/day/19 """
+
+from typing import Any, Dict, List, Tuple
+
+from adventofcode import LOG
+
+questions: List[str] = [
+    "",  # noqa: E501
+    "",  # noqa: E501
+]
+
+
+def part_one(**kwargs: Dict[str, Any]) -> Tuple[str, str]:
+    LOG.debug(f"part_one({kwargs=})")
+    answer: int = 0
+    return questions[0], answer
+
+
+def part_two(**kwargs: Dict[str, Any]) -> Tuple[str, str]:
+    LOG.debug(f"part_two({kwargs=})")
+    answer: int = 0
+    return questions[1], answer
